b31e4ec9d7ff84ed50e71514d92798d8292f119c
Akin Mert Bur <akinmertbur@gmail.com>
2024-04-30 22:47:36 +0300

    add API endpoint for updating existing blog posts in the Blog API

--- index.js
+++ index.js
@@ -64,6 +64,20 @@ app.post("/posts", (req, res) => {
 });
 
 //4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const existingPost = posts.find((post) => post.id === id);
+  const replacementPost = {
+    id: id,
+    title: req.body.title || existingPost.title,
+    content: req.body.content || existingPost.content,
+    author: req.body.author || existingPost.author,
+  };
+  const searchIndex = posts.findIndex((post) => post.id === id);
+  posts[searchIndex] = replacementPost;
+
+  res.json(replacementPost);
+});
 
 //5: DELETE a specific post by providing the post id.
 
